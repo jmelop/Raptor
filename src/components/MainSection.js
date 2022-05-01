f6f5028a31587d3a92a0616f2c914faf288e537e
Juan Melo Pérez <juanmlstm@gmail.com>
2022-05-01 14:18:06 +0100

    Navbar component changed

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -4,7 +4,7 @@ import './MainSection.css';
 
 function MainSection() {
     return (
-        <div className="relative bg-white overflow-hidden my-12">
+        <div className="relative bg-white overflow-hidden">
             <div className="pt-16 pb-80 sm:pt-24 sm:pb-40 lg:pt-40 lg:pb-48">
                 <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 sm:static">
                     <div className="sm:max-w-lg">
@@ -17,7 +17,7 @@ function MainSection() {
                         </p>
                     </div>
                     <div>
-                        <div className="mt-10">
+                        <div className="mt-20">
                             {/* Decorative image grid */}
                             <div aria-hidden="true"
                                 className="pointer-events-none lg:absolute lg:inset-y-0 lg:max-w-7xl lg:mx-auto lg:w-full">
@@ -27,47 +27,32 @@ function MainSection() {
                                             <div className="w-44 h-64 rounded-lg overflow-hidden sm:opacity-0 lg:opacity-100">
                                                 <img src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-01.jpg"
                                                     alt=""
-                                                    className="w-full h-full object-center object-cover"/>
-                                            </div>
-                                            <div className="w-44 h-64 rounded-lg overflow-hidden">
-                                                <img src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-02.jpg"
-                                                    alt=""
-                                                    className="w-full h-full object-center object-cover"/>
+                                                    className="w-full h-full object-center object-cover" />
                                             </div>
                                         </div>
                                         <div className="flex-shrink-0 grid grid-cols-1 gap-y-6 lg:gap-y-8">
                                             <div className="w-44 h-64 rounded-lg overflow-hidden">
                                                 <img src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-03.jpg"
                                                     alt=""
-                                                    className="w-full h-full object-center object-cover"/>
+                                                    className="w-full h-full object-center object-cover" />
                                             </div>
                                             <div className="w-44 h-64 rounded-lg overflow-hidden">
                                                 <img src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-04.jpg"
                                                     alt=""
-                                                    className="w-full h-full object-center object-cover"/>
-                                            </div>
-                                            <div className="w-44 h-64 rounded-lg overflow-hidden">
-                                                <img src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-05.jpg"
-                                                    alt=""
-                                                    className="w-full h-full object-center object-cover"/>
+                                                    className="w-full h-full object-center object-cover" />
                                             </div>
                                         </div>
                                         <div className="flex-shrink-0 grid grid-cols-1 gap-y-6 lg:gap-y-8">
                                             <div className="w-44 h-64 rounded-lg overflow-hidden">
                                                 <img src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-06.jpg"
                                                     alt=""
-                                                    className="w-full h-full object-center object-cover"/>
-                                            </div>
-                                            <div className="w-44 h-64 rounded-lg overflow-hidden">
-                                                <img src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-07.jpg"
-                                                    alt=""
-                                                    className="w-full h-full object-center object-cover"/>
+                                                    className="w-full h-full object-center object-cover" />
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <a href="#" className="inline-block text-center bg-indigo-600 border border-transparent rounded-md py-3 
+                            <a href="/products" className="inline-block text-center bg-indigo-600 border border-transparent rounded-md py-3 
                             px-8 font-medium text-white hover:bg-indigo-700">
                                 Shop Collection
                             </a>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,148 +1,167 @@
 import './Navbar.css';
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import { Fragment, useState } from 'react'
+import { Dialog, Popover, Transition } from '@headlessui/react'
+import { MenuIcon, SearchIcon, ShoppingBagIcon, XIcon } from '@heroicons/react/outline'
 
-const navigation = [
-    { name: 'Shop', href: '/', current: true },
-    { name: 'Products', href: '/products', current: false },
-    { name: 'Projects', href: '/projects', current: false },
-    { name: 'About Us', href: '/about-us', current: false },
-]
-
-function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
+const navigation = {
+    pages: [
+        { name: 'Shop', href: '/' },
+        { name: 'Products', href: '/products' },
+        { name: 'Projects', href: '/projects' },
+        { name: 'AboutUs', href: '/about-us' },
+    ],
 }
 
 function Navbar() {
+    const [open, setOpen] = useState(false)
     return (
-        <Disclosure as="nav" className="bg-gray-800">
-            {({ open }) => (
-                <>
-                    <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-                        <div className="relative flex items-center justify-between h-16">
-                            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                                {/* Mobile menu button*/}
-                                <Disclosure.Button className="inline-flex items-center justify-center 
-                                p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none 
-                                focus:ring-2 focus:ring-inset focus:ring-white">
-                                    <span className="sr-only">Open main menu</span>
-                                    {open ? (
-                                        <XIcon className="block h-6 w-6" aria-hidden="true" />
-                                    ) : (
-                                        <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                                    )}
-                                </Disclosure.Button>
-                            </div>
-                            <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                                <div className="flex-shrink-0 flex items-center">
-                                    <img
-                                        className="block lg:hidden h-8 w-auto"
-                                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                                        alt="Workflow" />
-                                    <img
-                                        className="hidden lg:block h-8 w-auto"
-                                        src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-                                        alt="Workflow" />
-                                </div>
-                                <div className="hidden sm:block sm:ml-6">
-                                    <div className="flex space-x-4">
-                                        {navigation.map((item) => (
-                                            <a
-                                                key={item.name}
-                                                href={item.href}
-                                                className={classNames(
-                                                    item.current ? 'bg-gray-900 text-white' :
-                                                        'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                                    'px-3 py-2 rounded-md text-sm font-medium'
-                                                )}
-                                                aria-current={item.current ? 'page' : undefined}>
-                                                {item.name}
-                                            </a>
-                                        ))}
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+        <div className="bg-white">
+            {/* Mobile menu */}
+            <Transition.Root show={open} as={Fragment}>
+                <Dialog as="div" className="fixed inset-0 flex z-40 lg:hidden" onClose={setOpen}>
+                    <Transition.Child
+                        as={Fragment}
+                        enter="transition-opacity ease-linear duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="transition-opacity ease-linear duration-300"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0">
+                        <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-25" />
+                    </Transition.Child>
+                    <Transition.Child
+                        as={Fragment}
+                        enter="transition ease-in-out duration-300 transform"
+                        enterFrom="-translate-x-full"
+                        enterTo="translate-x-0"
+                        leave="transition ease-in-out duration-300 transform"
+                        leaveFrom="translate-x-0"
+                        leaveTo="-translate-x-full">
+                        <div className="relative max-w-xs w-full bg-white shadow-xl pb-12 flex flex-col overflow-y-auto">
+                            <div className="px-4 pt-5 pb-2 flex">
                                 <button
                                     type="button"
-                                    className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white 
-                                    focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                                    <span className="sr-only">View notifications</span>
-                                    <BellIcon className="h-6 w-6" aria-hidden="true" />
+                                    className="-m-2 p-2 rounded-md inline-flex items-center justify-center text-gray-400"
+                                    onClick={() => setOpen(false)}>
+                                    <span className="sr-only">Close menu</span>
+                                    <XIcon className="h-6 w-6" aria-hidden="true" />
                                 </button>
-
-                                {/* Profile dropdown */}
-                                <Menu as="div" className="ml-3 relative">
-                                    <div>
-                                        <Menu.Button className="bg-gray-800 flex text-sm rounded-full 
-                                        focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                                            <span className="sr-only">Open user menu</span>
-                                            <img
-                                                className="h-8 w-8 rounded-full"
-                                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&i
-                                                xid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                                alt=""
-                                            />
-                                        </Menu.Button>
+                            </div>
+                            <div className="border-t border-gray-200 py-6 px-4 space-y-6">
+                                {navigation.pages.map((page) => (
+                                    <div key={page.name} className="flow-root">
+                                        <a href={page.href} className="-m-2 p-2 block font-medium text-gray-900">
+                                            {page.name}
+                                        </a>
                                     </div>
-                                    <Transition
-                                        as={Fragment}
-                                        enter="transition ease-out duration-100"
-                                        enterFrom="transform opacity-0 scale-95"
-                                        enterTo="transform opacity-100 scale-100"
-                                        leave="transition ease-in duration-75"
-                                        leaveFrom="transform opacity-100 scale-100"
-                                        leaveTo="transform opacity-0 scale-95">
-                                        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 
-                                        rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a href="#" className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
-                                                        Your Profile
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a href="#" className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
-                                                        Settings
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a href="#" className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
-                                                        Sign out
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                        </Menu.Items>
-                                    </Transition>
-                                </Menu>
+                                ))}
+                            </div>
+                            <div className="border-t border-gray-200 py-6 px-4 space-y-6">
+                                <div className="flow-root">
+                                    <a href="/" className="-m-2 p-2 block font-medium text-gray-900">
+                                        Sign in
+                                    </a>
+                                </div>
+                                <div className="flow-root">
+                                    <a href="/" className="-m-2 p-2 block font-medium text-gray-900">
+                                        Create account
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="border-t border-gray-200 py-6 px-4">
+                                <a href="/" className="-m-2 p-2 flex items-center">
+                                    <img
+                                        src="https://tailwindui.com/img/flags/flag-canada.svg"
+                                        alt=""
+                                        className="w-5 h-auto block flex-shrink-0" />
+                                    <span className="ml-3 block text-base font-medium text-gray-900">CAD</span>
+                                    <span className="sr-only">, change currency</span>
+                                </a>
+                            </div>
+                        </div>
+                    </Transition.Child>
+                </Dialog>
+            </Transition.Root>
+            <header className="relative bg-white">
+                <p className="bg-indigo-600 h-10 flex items-center justify-center text-sm font-medium text-white px-4 sm:px-6 lg:px-8">
+                    Get free delivery on orders over $100
+                </p>
+                <nav aria-label="Top" className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <div className="border-b border-gray-200">
+                        <div className="h-16 flex items-center">
+                            <button
+                                type="button"
+                                className="bg-white p-2 rounded-md text-gray-400 lg:hidden"
+                                onClick={() => setOpen(true)}>
+                                <span className="sr-only">Open menu</span>
+                                <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                            </button>
+                            {/* Logo */}
+                            <div className="ml-4 flex lg:ml-0">
+                                <a href="/">
+                                    <span className="sr-only">Workflow</span>
+                                    <img
+                                        className="h-8 w-auto"
+                                        src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600"
+                                        alt="" />
+                                </a>
+                            </div>
+                            {/* Flyout menus */}
+                            <Popover.Group className="hidden lg:ml-8 lg:block lg:self-stretch">
+                                <div className="h-full flex space-x-8">
+                                    {navigation.pages.map((page) => (
+                                        <a
+                                            key={page.name}
+                                            href={page.href}
+                                            className="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800">
+                                            {page.name}
+                                        </a>
+                                    ))}
+                                </div>
+                            </Popover.Group>
+                            <div className="ml-auto flex items-center">
+                                <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+                                    <a href="/" className="text-sm font-medium text-gray-700 hover:text-gray-800">
+                                        Sign in
+                                    </a>
+                                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                                    <a href="/" className="text-sm font-medium text-gray-700 hover:text-gray-800">
+                                        Create account
+                                    </a>
+                                </div>
+                                <div className="hidden lg:ml-8 lg:flex">
+                                    <a href="/" className="text-gray-700 hover:text-gray-800 flex items-center">
+                                        <img
+                                            src="https://tailwindui.com/img/flags/flag-canada.svg"
+                                            alt=""
+                                            className="w-5 h-auto block flex-shrink-0" />
+                                        <span className="ml-3 block text-sm font-medium">CAD</span>
+                                        <span className="sr-only">, change currency</span>
+                                    </a>
+                                </div>
+                                {/* Search */}
+                                <div className="flex lg:ml-6">
+                                    <a href="/" className="p-2 text-gray-400 hover:text-gray-500">
+                                        <span className="sr-only">Search</span>
+                                        <SearchIcon className="w-6 h-6" aria-hidden="true" />
+                                    </a>
+                                </div>
+                                {/* Cart */}
+                                <div className="ml-4 flow-root lg:ml-6">
+                                    <a href="/" className="group -m-2 p-2 flex items-center">
+                                        <ShoppingBagIcon
+                                            className="flex-shrink-0 h-6 w-6 text-gray-400 group-hover:text-gray-500"
+                                            aria-hidden="true" />
+                                        <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">0</span>
+                                        <span className="sr-only">items in cart, view bag</span>
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <Disclosure.Panel className="sm:hidden">
-                        <div className="px-2 pt-2 pb-3 space-y-1">
-                            {navigation.map((item) => (
-                                <Disclosure.Button
-                                    key={item.name}
-                                    as="a"
-                                    href={item.href}
-                                    className={classNames(
-                                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                        'block px-3 py-2 rounded-md text-base font-medium'
-                                    )}
-                                    aria-current={item.current ? 'page' : undefined}>
-                                    {item.name}
-                                </Disclosure.Button>
-                            ))}
-                        </div>
-                    </Disclosure.Panel>
-                </>
-            )}
-        </Disclosure>
+                </nav>
+            </header>
+        </div>
     )
 }
 
