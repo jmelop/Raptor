c6236217e1f7218af1a4638ddd3cfbd45a2fa53d
Juan Melo Pérez <juanmlstm@gmail.com>
2022-05-13 18:25:16 +0100

    Added responsive styles in about us component

--- src/components/Team.js
+++ src/components/Team.js
@@ -6,13 +6,13 @@ import './Team.css';
 function Team() {
     return (
         <>
-            <div className="flex flex-col">
-                <div className="justify-center mx-auto my-24">
+            <div className="relative">
+                <div className="justify-center mx-auto my-24 max-w-2xl px-4 sm:px-6 lg:max-w-5xl lg:px-8">
                     <div className="mb-10">
                         <h1 className="font-bold text-2xl">- Our Team -</h1>
                         <h3 className="mt-2 font-medium text-gray-600">Phasellus eget tincidunt enim. Pellentesque ut mauris nec lacus luctus fringilla nec a tortor.</h3>
                     </div>
-                    <div className="inline-grid grid-cols-3 gap-4">
+                    <div className="relative grid grid-cols justify-center gap-y-8 sm:grid-cols-2 gap-x-2 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-6">
                         <div className="bg-slate-800 rounded-md w-72">
                             <Member
                                 src='images/p1.jpg'
