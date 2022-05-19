fbdddba7ee758e1e547ab66abae7e18369d5dd44
Juan Melo Pérez <juanmlstm@gmail.com>
2022-05-19 12:02:47 +0100

    Navbar styles changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -86,71 +86,69 @@ function Navbar() {
                     Get free delivery on orders over $100
                 </p>
                 <nav aria-label="Top" className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div className="border-b border-gray-200">
-                        <div className="h-16 flex items-center">
-                            <button
-                                type="button"
-                                className="bg-white p-2 rounded-md text-gray-400 lg:hidden"
-                                onClick={() => setOpen(true)}>
-                                <span className="sr-only">Open menu</span>
-                                <MenuIcon className="h-6 w-6" aria-hidden="true" />
-                            </button>
-                            {/* Logo */}
-                            <div className="ml-4 flex lg:ml-0">
-                                <a href="/">
-                                    <span className="sr-only">Dron</span>
-                                    <img
-                                        className="h-8 w-auto"
-                                        src="images/drone.png"
-                                        alt="" />
-                                </a>
-                            </div>
-                            {/* Flyout menus */}
-                            <Popover.Group className="hidden lg:ml-8 lg:block lg:self-stretch">
-                                <div className="h-full flex space-x-8">
-                                    {navigation.pages.map((page) => (
-                                        <a
-                                            key={page.name}
-                                            href={page.href}
-                                            className="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800">
-                                            {page.name}
-                                        </a>
-                                    ))}
-                                </div>
-                            </Popover.Group>
-                            <div className="ml-auto flex items-center">
-                                <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
-                                    <a href="/login" className="text-sm font-medium text-gray-700 hover:text-gray-800">
-                                        Sign in
-                                    </a>
-                                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
-                                </div>
-                                <div className="hidden lg:ml-8 lg:flex">
-                                    <a href="/" className="text-gray-700 hover:text-gray-800 flex items-center">
+                    <div className="h-16 flex items-center">
+                        <button
+                            type="button"
+                            className="bg-white p-2 rounded-md text-gray-400 lg:hidden"
+                            onClick={() => setOpen(true)}>
+                            <span className="sr-only">Open menu</span>
+                            <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                        </button>
+                        <div className='relative w-full'>
+                            <div className='grid grid-cols-1 mt-10 gap-x-4 md:grid-cols-1 lg:grid-cols-3 px-4'>
+                                <Popover.Group className="hidden lg:ml-8 lg:block lg:self-stretch mb-4 border-b border-gray-200">
+                                    <div className="h-full flex space-x-8">
+                                        {navigation.pages.map((page) => (
+                                            <a
+                                                key={page.name}
+                                                href={page.href}
+                                                className="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800">
+                                                {page.name}
+                                            </a>
+                                        ))}
+                                    </div>
+                                </Popover.Group>
+                                <div className="hidden lg:ml-4 lg:flex lg:ml-0 justify-center mb-4">
+                                    <a href="/">
+                                        <span className="sr-only">Dron</span>
                                         <img
-                                            src="https://tailwindui.com/img/flags/flag-canada.svg"
-                                            alt=""
-                                            className="w-5 h-auto block flex-shrink-0" />
-                                        <span className="ml-3 block text-sm font-medium">CAD</span>
-                                        <span className="sr-only">, change currency</span>
+                                            className="h-8 w-auto"
+                                            src="images/drone.png"
+                                            alt="" />
                                     </a>
                                 </div>
-                                {/* Search */}
-                                <div className="flex lg:ml-6">
-                                    <a href="/" className="p-2 text-gray-400 hover:text-gray-500">
-                                        <span className="sr-only">Search</span>
-                                        <SearchIcon className="w-6 h-6" aria-hidden="true" />
-                                    </a>
-                                </div>
-                                {/* Cart */}
-                                <div className="ml-4 flow-root lg:ml-6">
-                                    <a href="/" className="group -m-2 p-2 flex items-center">
-                                        <ShoppingBagIcon
-                                            className="flex-shrink-0 h-6 w-6 text-gray-400 group-hover:text-gray-500"
-                                            aria-hidden="true" />
-                                        <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">0</span>
-                                        <span className="sr-only">items in cart, view bag</span>
-                                    </a>
+                                <div className="ml-auto flex items-center mb-4 w-full border-b border-gray-200">
+                                    <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+                                        <a href="/login" className="text-sm font-medium text-gray-700 hover:text-gray-800">
+                                            Sign in
+                                        </a>
+                                        <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                                    </div>
+                                    <div className="hidden lg:ml-8 lg:flex">
+                                        <a href="/" className="text-gray-700 hover:text-gray-800 flex items-center">
+                                            <img
+                                                src="https://tailwindui.com/img/flags/flag-canada.svg"
+                                                alt=""
+                                                className="w-5 h-auto block flex-shrink-0" />
+                                            <span className="ml-3 block text-sm font-medium">CAD</span>
+                                            <span className="sr-only">, change currency</span>
+                                        </a>
+                                    </div>
+                                    <div className="hidden lg:ml-6 lg:flex">
+                                        <a href="/" className="p-2 text-gray-400 hover:text-gray-500">
+                                            <span className="sr-only">Search</span>
+                                            <SearchIcon className="w-6 h-6" aria-hidden="true" />
+                                        </a>
+                                    </div>
+                                    <div className="hidden lg:ml-4 lg:flex">
+                                        <a href="/" className="group -m-2 p-2 flex items-center">
+                                            <ShoppingBagIcon
+                                                className="flex-shrink-0 h-6 w-6 text-gray-400 group-hover:text-gray-500"
+                                                aria-hidden="true" />
+                                            <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">0</span>
+                                            <span className="sr-only">items in cart, view bag</span>
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
